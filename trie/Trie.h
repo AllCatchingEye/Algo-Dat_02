2c332a00bfbad8e320272aefbc74d2a19cdfdded
hm-glang <glang@hm.edu>
2021-12-11 12:36:57 +0100

    Implemented Subclasses Node, Branch and Leaf

--- trie/Trie.h
+++ trie/Trie.h
@@ -1,9 +1,9 @@
 /*
- * Trie.h
- *
- *  Created on: 17.09.2020
- *      Author: Max Fischer
- */
+* Trie.h
+*
+*  Created on: 17.09.2020
+*      Author: Max Fischer
+*/
 
 #ifndef SRC_TRIE_H_
 #define SRC_TRIE_H_
@@ -15,81 +15,124 @@ using namespace std;
 template <class T, class E=char>
 class Trie {
 public:
-	typedef basic_string<E> key_type; //string=basic_string<char>
-	typedef pair<const key_type, T> value_type;
-	typedef T mapped_type;
+ typedef basic_string<E> key_type; //string=basic_string<char>
+ typedef pair<const key_type, T> value_type;
+ typedef T mapped_type;
 
+private:
+ static const E FINAL_SYMBOL = '\0';
 
-	class TrieIterator {
-		typedef TrieIterator iterator;
+ class Node {
+  public:
 
-	public:
-		TrieIterator() {
-		}
+   virtual ~Node() = default;
+   virtual bool empty() const = 0;
+ };
 
-		value_type operator *() {
-		    return value_type(); //FIXME Dummy-Implementierung korrigieren
-		}
+ class Leaf : public Node {
+  public:
+   value_type data;
 
-		bool operator !=(const iterator& rhs) const {
-		    auto tmp(rhs);      //FIXME Dummy-Implementierung korrigieren
-		    return false;       //FIXME Dummy-Implementierung korrigieren
- 		}
+   explicit Leaf(value_type data) : data(data) {
 
-		bool operator ==(const iterator& rhs) const {
-		    auto tmp(rhs);      //FIXME Dummy-Implementierung korrigieren
-		    return false;       //FIXME Dummy-Implementierung korrigieren
-		}
+   }
 
-		iterator& operator ++() {
-		    static iterator res; //FIXME Dummy-Implementierung korrigieren
-		    return res;          //FIXME Dummy-Implementierung korrigieren
-		}
+   ~Leaf() = default;
 
-		iterator operator ++(int) { // postfix operator, dummy parameter
-		    return iterator();  //FIXME Dummy-Implementierung korrigieren
-		}
-	};
+   bool empty() const override {
+     return false;
+   }
+ };
 
-	Trie() {
-	}
+ class Branch: public Node {
+  public:
+   map<E, Node*> children;
 
-	~Trie() {
-	}
+   ~Branch() {
+     for (auto it = children.begin(); it != children.end(); it++) {
+       delete it->second;
+     }
+   }
 
-	typedef TrieIterator iterator;
+   bool empty() const override {
+     return children.empty();
+   }
+ };
 
-	bool empty() const {
-	    return true;    //FIXME Dummy-Implementierung korrigieren
-	}
+ Branch *root;
 
-	void insert(const value_type& value) {
-	    auto tmp(value); //FIXME Dummy-Implementierung korrigieren
-	}
+public:
 
-	void erase(const key_type& value) {
-	    auto tmp(value); //FIXME Dummy-Implementierung korrigieren
-	}
+ class TrieIterator {
+   typedef TrieIterator iterator;
 
-	void clear() {
-	}
+  public:
+   TrieIterator() {
+   }
 
-	iterator find(const key_type& testElement) {
-  	    auto tmp(testElement); 
-	    return iterator();  //FIXME Dummy-Implementierung korrigieren
-	}
+   value_type operator *() {
+     return value_type(); //FIXME Dummy-Implementierung korrigieren
+   }
 
-	iterator begin() {
-	    return iterator();  //FIXME Dummy-Implementierung korrigieren
-	}
+   bool operator !=(const iterator& rhs) const {
+     auto tmp(rhs);      //FIXME Dummy-Implementierung korrigieren
+     return false;       //FIXME Dummy-Implementierung korrigieren
+   }
 
-	iterator end() {
-	    return iterator();  //FIXME Dummy-Implementierung korrigieren
-	}
+   bool operator ==(const iterator& rhs) const {
+     auto tmp(rhs);      //FIXME Dummy-Implementierung korrigieren
+     return false;       //FIXME Dummy-Implementierung korrigieren
+   }
 
-	void printOn(ostream& ostr) const {
-	  ostr << std::endl;   //FIXME Dummy-Implementierung korrigieren
-	}
+   iterator& operator ++() {
+     static iterator res; //FIXME Dummy-Implementierung korrigieren
+     return res;          //FIXME Dummy-Implementierung korrigieren
+   }
+
+   iterator operator ++(int) { // postfix operator, dummy parameter
+     return iterator();  //FIXME Dummy-Implementierung korrigieren
+   }
+ };
+
+ Trie() {
+ }
+
+ ~Trie() {
+ }
+
+ typedef TrieIterator iterator;
+
+ bool empty() const {
+   return true;    //FIXME Dummy-Implementierung korrigieren
+ }
+
+ void insert(const value_type& value) {
+   auto tmp(value); //FIXME Dummy-Implementierung korrigieren
+ }
+
+ void erase(const key_type& value) {
+   auto tmp(value); //FIXME Dummy-Implementierung korrigieren
+ }
+
+ void clear() {
+ }
+
+ iterator find(const key_type& testElement) {
+   auto tmp(testElement);
+   return iterator();  //FIXME Dummy-Implementierung korrigieren
+ }
+
+ iterator begin() {
+   return iterator();  //FIXME Dummy-Implementierung korrigieren
+ }
+
+ iterator end() {
+   return iterator();  //FIXME Dummy-Implementierung korrigieren
+ }
+
+ void printOn(ostream& ostr) const {
+   ostr << std::endl;   //FIXME Dummy-Implementierung korrigieren
+ }
 };
 
 template <class E>
